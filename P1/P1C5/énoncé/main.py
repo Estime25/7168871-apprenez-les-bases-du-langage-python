18edb730405d2217488ec646a7cb928116a26ffb
Estime25 <146753696+Estime25@users.noreply.github.com>
2025-11-29 18:15:00 +0100

    Initialize variables and print their values and types
    Added variables for name, age, height, and student status, along with print statements to display their values and types.

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
-# Écrivez votre code ici !
+Nom = "david"
+Age = 20
+Taille = 1.90
+est_etudiant = True
+print (f"Nom: {Nom}")
+print (f"Age: {Age}")
+print (f"Taille: {Taille}")
+print (f"est_etudiant: {est_etudiant}")
+
+print(f"Type nom: {type(Nom)}")
+print(f"Type age: {type(Age)}")
+print(f"Type taille: {type(Taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
